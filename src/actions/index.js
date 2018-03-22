7b2879f519175c9ce352a0920703e9a473670d5a
markupua <psdhtmlcss@gmail.com>
2018-03-22 20:32:49 +0200

    was fixed @flow problems for actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,36 @@
+// @flow
+
 export function openDrawer() {
-  return { 
+  return {
     type: 'OPEN_DRAWER',
     drawer: true
-  }
+  };
 }
 
 export function closeDrawer() {
-  return { 
+  return {
     type: 'CLOSE_DRAWER',
     drawer: false
-  }
+  };
 }
 
-export function toggleStatus(val) {
-  return { 
+export function toggleStatus(val: string) {
+  return {
     type: 'TOGGLE_STATUS',
     status: val
-  }
+  };
 }
 
-export function setUsername(val) {
+export function setUsername(val: string) {
   return {
     type: 'SET_USERNAME',
     name: val
-  }
+  };
 }
 
-export function toggleStudentTab(val) {
+export function toggleStudentTab(val: boolean) {
   return {
     type: 'TOGGLE_STUDENT_TAB',
     studentTab: val
-  }
+  };
 }
