c0bb06ac694f7e6a188c47b3a0a3e54501884297
markupua <psdhtmlcss@gmail.com>
2018-04-05 22:45:43 +0300

    was updated stores files

--- src/stores/classesTempData.js
+++ src/stores/classesTempData.js
@@ -1,35 +1,40 @@
-export default [
-    {
-        "id": "1",
-        "title": "Основы древнерусской иконописи",
-        "city": "Саратов",
-        "teacher": "П.С. Штраубе", 
-        "description": "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.",
-        "accepted": "false"
-    },
-    {
-        "id": "2",
-        "title": "Перспективы развития Тамбовской области",
-        "city": "Тамбов",
-        "teacher": "А.Б. Штейн", 
-        "description": "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.",
-        "accepted": "true"
-    },
-    {
-        "id": "3",
-        "title": "Органическое сайтостроение",
-        "city": "Челябинск",
-        "teacher": "Е.А. Поляков", 
-        "description": "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.",
-        "accepted": "false"
-    },
-    {
-        "id": "4",
-        "title": "Здравствуйте, я ваша Jira",
-        "city": "Санкт-Петербург",
-        "teacher": "К.Б. Крупович", 
-        "description": "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.",
-        "accepted": "false"
-    },
-]
+// @flow
 
+export default [
+  {
+    id: '1',
+    title: 'Основы древнерусской иконописи',
+    city: 'Саратов',
+    teacher: 'П.С. Штраубе',
+    description:
+      'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+    accepted: 'false'
+  },
+  {
+    id: '2',
+    title: 'Перспективы развития Тамбовской области',
+    city: 'Тамбов',
+    teacher: 'А.Б. Штейн',
+    description:
+      'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+    accepted: 'true'
+  },
+  {
+    id: '3',
+    title: 'Органическое сайтостроение',
+    city: 'Челябинск',
+    teacher: 'Е.А. Поляков',
+    description:
+      'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+    accepted: 'false'
+  },
+  {
+    id: '4',
+    title: 'Здравствуйте, я ваша Jira',
+    city: 'Санкт-Петербург',
+    teacher: 'К.Б. Крупович',
+    description:
+      'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+    accepted: 'false'
+  }
+];
--- src/stores/studentsTempData.js
+++ src/stores/studentsTempData.js
@@ -1,63 +1,81 @@
-export default [
-    {
-        "id": "1",
-        "name": "Стас Фёдоров",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "2",
-        "name": "Сергей Петров",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "3",
-        "name": "Дмитрий Козлов",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "5",
-        "name": "Анна Мирошниченко",
-        "faculty": "Филологии",
-        "accepted": "true"
-    },
-    {
-        "id": "6",
-        "name": "Пётр Манов",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "7",
-        "name": "Дарья Семенченко",
-        "faculty": "Филологии",
-        "accepted": "true"
-    },
-    {
-        "id": "8",
-        "name": "Сигизмунд Кузькин",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "9",
-        "name": "Епифан Обормотов",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-    {
-        "id": "10",
-        "name": "Владлен Коровин",
-        "faculty": "Филологии",
-        "accepted": "true"
-    },
-    {
-        "id": "11",
-        "name": "Иван Ганин",
-        "faculty": "Филологии",
-        "accepted": "false"
-    },
-]
+// @flow
 
+export default [
+  {
+    id: '0',
+    account: 'Гость',
+    name: 'Гость',
+    faculty: 'NaN',
+    accepted: 'false'
+  },
+  {
+    id: '1',
+    account: 'Stas',
+    name: 'Стас Фёдоров',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '2',
+    account: 'Sergey',
+    name: 'Сергей Петров',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '3',
+    account: 'Dima',
+    name: 'Дмитрий Козлов',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '5',
+    account: 'Anna',
+    name: 'Анна Мирошниченко',
+    faculty: 'Филологии',
+    accepted: 'true'
+  },
+  {
+    id: '6',
+    account: 'Petya',
+    name: 'Пётр Манов',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '7',
+    account: 'Dasha',
+    name: 'Дарья Семенченко',
+    faculty: 'Филологии',
+    accepted: 'true'
+  },
+  {
+    id: '8',
+    account: 'Kuzya',
+    name: 'Сигизмунд Кузькин',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '9',
+    account: 'Efpian',
+    name: 'Епифан Обормотов',
+    faculty: 'Филологии',
+    accepted: 'false'
+  },
+  {
+    id: '10',
+    account: 'Vladlen',
+    name: 'Владлен Коровин',
+    faculty: 'Филологии',
+    accepted: 'true'
+  },
+  {
+    id: '11',
+    account: 'Ivan',
+    name: 'Иван Ганин',
+    faculty: 'Филологии',
+    accepted: 'false'
+  }
+];
