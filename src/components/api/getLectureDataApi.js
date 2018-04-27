a8e8291966a97fd40fb08dce78c5e4705c4799c3
markupua <psdhtmlcss@gmail.com>
2018-04-27 12:53:17 +0300

    was created get lecture data api

---
+++ src/components/api/getLectureDataApi.js
@@ -0,0 +1,40 @@
+// @flow
+import fetch from 'isomorphic-fetch';
+
+export default function getLectureDataApi(vkUrl: string) {
+  let urlData = vkUrl.split('-')[1];
+  let groupId = urlData.split('_')[0];
+  let topicId = urlData.split('_')[1];
+  let url =
+    'https://api.vk.com/method/board.getTopics?group_id=' +
+    groupId +
+    '&topic_ids=' +
+    topicId +
+    '&preview=1&preview_length=0&extended=1&v=5.45';
+  return fetch('https://cors-anywhere.herokuapp.com/' + url, {
+    mode: 'cors'
+  })
+    .then(response => {
+      if (response.status !== 200) {
+        alert(
+          'Looks like there was a problem. Status Code: ' + response.status
+        );
+        return;
+      }
+      return response.json();
+    })
+    .then(data => {
+      let lectureData = {
+        title: data.response.items[0].title,
+        text: data.response.items[0].first_comment,
+        closed: data.response.items[0].is_closed,
+        teachername:
+          data.response.profiles[0].first_name +
+          ' ' +
+          data.response.profiles[0].last_name,
+        teacherphoto: data.response.profiles[0].photo_100
+      };
+      return lectureData;
+    })
+    .catch(error => error);
+}
