cceb5a0c88042490492a5aa65b417ff047fda94c
markupua <psdhtmlcss@gmail.com>
2018-05-17 21:31:33 +0300

    was created localstorage

--- src/stores/storeConfig.js
+++ src/stores/storeConfig.js
@@ -1,9 +1,14 @@
 // @flow
 
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import persistState from 'redux-localstorage';
 import rootReducer from '../reducers';
 
 export default function schoolStore(initialState: any) {
-  const store = createStore(rootReducer, initialState);
+  const enhancer = compose(
+    /* [middlewares] */
+    persistState(/*paths, config*/)
+  );
+  const store = createStore(rootReducer, initialState, enhancer);
   return store;
 }
